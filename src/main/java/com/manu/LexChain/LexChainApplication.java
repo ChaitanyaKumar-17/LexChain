7b92da57f85099c54bce2301246c945b4f394a01
Chaitanya Kumar <chaitanya0817@gmail.com>
2026-04-09 19:48:01 +0530

    initial commit

---
+++ src/main/java/com/manu/LexChain/LexChainApplication.java
@@ -0,0 +1,13 @@
+package com.manu.LexChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manu/LexChain/LexChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu.LexChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
