73b698a16675d7e7e72367f75941d755c124e7b8
Chaitanya Kumar <chaitanya0817@gmail.com>
2026-04-17 13:27:29 +0530

    removed unused code

--- backend/src/main/java/com/manu/LexChain/repository/WhitelistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.manu.LexChain.repository;
-
-import com.manu.LexChain.model.CreatorWhitelist;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WhitelistRepository extends MongoRepository<CreatorWhitelist, String> {
-    boolean existsByWalletAddressIgnoreCase(String walletAddress);
-}
